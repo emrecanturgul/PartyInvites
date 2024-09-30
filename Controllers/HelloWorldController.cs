617e9ec399f6e3caf70b0ac28a1c3c720fbaa12a
Noel Lopes <noel@ipg.pt>
2024-09-30 17:14:48 +0100

    Add an HelloWorld controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "Hello World !!!";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "Welcome to the Hello World controller";
+        }
+    }
+}
