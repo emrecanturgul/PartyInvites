567852013f08707f0bc754c03a329fcf88d464df
Noel Lopes <noel@ipg.pt>
2024-10-07 15:14:58 +0100

    Show the time of the day at the location of the server

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,9 +6,24 @@ namespace PartyInvites.Controllers
     {
         //
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello World !!!";
+            DateTime dateTime = DateTime.Now;
+
+            int hour = dateTime.Hour;
+
+            string timeOfDay;
+            if (hour >= 6 && hour <= 12) {
+                timeOfDay = "morning";
+            } else if (hour > 12 && hour < 20) {
+                timeOfDay = "afternoon";
+            } else {
+                timeOfDay = "night";
+            }
+
+            ViewBag.Message = $"Hello from the server. At the server location now is {timeOfDay}.";
+
+            return View();
         }
 
         //
